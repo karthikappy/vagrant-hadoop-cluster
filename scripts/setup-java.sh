5d23116a113a09ed3eaec5d49ca7c16e0b7c6050
Karthik Sankaran <ks584@njit.edu>
2026-08-17 16:19:59 -0400

    Updated Java

--- scripts/setup-java.sh
+++ scripts/setup-java.sh
@@ -3,9 +3,9 @@
 echo "JAVA: Starting"
 echo "JAVA: Copying Files"
 #tar -xzf /vagrant/resources/software/openjdk-11.0.1_linux-x64_bin.tar.gz -C /usr/local
-tar -xzf /vagrant/resources/software/jdk-11.0.24_linux-x64_bin.tar.gz -C /usr/local
+tar -xzf /vagrant/resources/software/jdk-17.0.19_linux-x64_bin.tar.gz -C /usr/local
 #ln -s /usr/local/jdk-11.0.1 /usr/local/java
-ln -s /usr/local/jdk-11.0.24 /usr/local/java
+ln -s /usr/local/jdk-17.0.19 /usr/local/java
 update-alternatives --install /usr/bin/java java /usr/local/java/bin/java 1000
 update-alternatives --install /usr/bin/javac javac /usr/local/java/bin/javac 1000
 
